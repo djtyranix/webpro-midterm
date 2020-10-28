6aa71b23f1f1dc0ca485766232516791f3380cc7
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-10-28 23:28:50 +0700

    Full Project Upload, Standard UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,7 @@
                 <div class="col-md-12 text-center">
                     <img src="{{ URL::to('/') }}/images/logo.png" alt="logo" style="width: 12rem;" class="mt-5 mb-1">
                     <h1 style="font-size: 4rem;" class="mb-0">
-                        Welcome to {{config('app.name')}} 
+                        Welcome to {{config('app.name')}}
                     </h1>
                     <h1 style="font-size: 8rem;" class="mb-5">
                         <strong>Commander!</strong>
